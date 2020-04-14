f7c6346261caaacb4c45eff233dec63ecb388243
KacperMazurIT <kacper.mazur.it@gmail.com>
2020-04-14 10:09:12 +0200

    Utworzenie interfejsu do autoryzacji

---
+++ PortalRandkowy.API/Data/IAuthRepository.cs
@@ -0,0 +1,11 @@
+namespace PortalRandkowy.API.Data
+{
+    public interface IAuthRepository
+    {
+         Task<User> Login(string username, string password);
+
+         Task<User> Register(User user, string password);
+
+         Task<bool> UserExists(string username);
+    }
+}
